0dc1e30b2992696aa6f275ed48ea84ba80f48b52
unknown <zakaria.janah2018@gmail.com>
2024-01-13 16:37:53 +0100

    adding discriminator class with test

---
+++ Unit test/disc_test.py
@@ -0,0 +1,35 @@
+from Discriminator import Discriminator
+from Generator import Generator, get_noise
+
+num_test = 100
+
+gen = Generator()
+disc = Discriminator()
+test_images = gen(get_noise(num_test, gen.z_dim))
+
+test_hidden_block = disc.make_disc_block(1, 5, kernel_size=6, stride=3)
+hidden_output = test_hidden_block(test_images)
+
+test_final_block = disc.make_disc_block(1, 10, kernel_size=2, stride=5, final_layer=True)
+final_output = test_final_block(test_images)
+
+disc_output = disc(test_images)
+
+
+assert tuple(hidden_output.shape) == (num_test, 5, 8, 8)
+
+assert -hidden_output.min() / hidden_output.max() > 0.15
+assert -hidden_output.min() / hidden_output.max() < 0.25
+assert hidden_output.std() > 0.5
+assert hidden_output.std() < 1
+
+assert tuple(final_output.shape) == (num_test, 10, 6, 6)
+assert final_output.max() > 1.0
+assert final_output.min() < -1.0
+assert final_output.std() > 0.3
+assert final_output.std() < 0.6
+
+assert tuple(disc_output.shape) == (num_test, 1)
+assert disc_output.std() > 0.25
+assert disc_output.std() < 0.5
+print("Success!")
